9b811c6c4b7298cbba6e80a5317088b4d5dafea5
Denys Chepiha <dchepiha@exoft.net>
2023-06-04 14:01:23 +0200

    Create obs-app

---
+++ obs-app/app/index.js
@@ -0,0 +1,12 @@
+// Core
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
